11c60e255b95812f3dbc60013051904180f48a77
emiliojes <emiliojes@gmail.com>
2018-11-15 23:59:48 -0500

    add list in the project

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import sys
 
 
-clients = 'pablo,ricardo,'
+clients = ['pablo', 'ricardo']
 
 
 def create_client(client_name):
 	global clients
 
 	if client_name not in clients:
-		clients += client_name
-		_add_coma()
+		clients.append(client_name)
 	else:
 		print('Client already is in the client\'s list')
 
 
 def list_clients():
-	global clients
-	
-	print(clients)
+	for idx, client in enumerate(clients):
+		print('{}:{}'.format(idx, client))
 
 
-def update_client(client_name, updated_client_name):
+def update_client(client_name, updated_name):
 	global clients
 
 	if client_name in clients:
-		clients = clients.replace(client_name + ',', updated_client_name + ',')
+		index = clients.index(client_name)
+		clients[index] = updated_name
 	else:
 		print('Client is not in clients list')
 
@@ -33,26 +32,17 @@ def delete_client(client_name):
 	global clients
 
 	if client_name in clients:
-		clients = clients.replace(client_name + ',', '')
+		clients.remove(client_name)
 	else:
 		print('Client is not found in clients list')
 
 
 def search_client(client_name):
-	clients_list = clients.split(',')
-
-	for client in clients_list:
+	for client in clients:
 		if client != client_name:
 			continue
 		else: 
 			return True
-
-
-def _add_coma():
-	global clients
-	
-	clients += ','
-
 
 def _print_welcome():
 	print('WELCOME TO SALES... ')
